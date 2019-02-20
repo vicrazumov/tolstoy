380cdf28c22d6bbb0e3446cb1f48afa0801616a6
Viktor <vnrazumov@gmail.com>
2019-02-20 22:38:18 +0100

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,15 @@
 const path = require('path');
-const ExamplePlugin = require('./webpack-plugins/example')
 
 module.exports = {
   entry: './src/индекс.лн',
   mode: 'development',
-  // plugins: [new ExamplePlugin({ options: true })],
   module: {
     rules: [
       {
         test: /\.лн$/,
         use: [
           {
-            loader: path.resolve('./webpack-plugins/loader.js'),
+            loader: path.resolve('./webpack-plugins/tolstoy.js'),
             // options: {/* ... */}
           }
         ]
