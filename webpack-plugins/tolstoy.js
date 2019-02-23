0a881f1ccae83a7a3d544c37e9ca682c3e619e1f
Viktor <vnrazumov@gmail.com>
2019-02-23 14:46:52 +0100

    Support import/export

--- webpack-plugins/tolstoy.js
+++ webpack-plugins/tolstoy.js
@@ -10,6 +10,10 @@ const transpileMap = {
   'случайно': 'random',
   'функция': 'function',
   'переменная': 'let',
+  'импорт': 'import',
+  'экспорт': 'export',
+  'дефолт': 'default',
+  'из': 'from',
 }
 
 var transliterationMap = { "Ё": "YO", "Й": "I", "Ц": "TS", "У": "U", "К": "K", "Е": "E", "Н": "N", "Г": "G", "Ш": "SH", "Щ": "SCH", "З": "Z", "Х": "H", "Ъ": "", "ё": "yo", "й": "i", "ц": "ts", "у": "u", "к": "k", "е": "e", "н": "n", "г": "g", "ш": "sh", "щ": "sch", "з": "z", "х": "h", "ъ": "", "Ф": "F", "Ы": "I", "В": "V", "А": "A", "П": "P", "Р": "R", "О": "O", "Л": "L", "Д": "D", "Ж": "ZH", "Э": "E", "ф": "f", "ы": "i", "в": "v", "а": "a", "п": "p", "р": "r", "о": "o", "л": "l", "д": "d", "ж": "zh", "э": "e", "Я": "Ya", "Ч": "CH", "С": "S", "М": "M", "И": "I", "Т": "T", "Ь": "", "Б": "B", "Ю": "YU", "я": "ya", "ч": "ch", "с": "s", "м": "m", "и": "i", "т": "t", "ь": "", "б": "b", "ю": "yu" }
@@ -20,8 +24,13 @@ const translate = (text, map) => Object.entries(map).reduce((acc, [key, value]) 
 }, text)
 
 const extractStringLiterals = text => {
-  const literalsMap = text
-    .match(/\'.+\'/g)
+  const match = text.match(/\'.+\'/g)
+  if (!match) return {
+    literalsMap: {},
+    text,
+  }
+
+  const literalsMap = match
     .reduce((acc, next, idx) => {
       acc[next] = `!!STRING${idx}!!`;
       return acc
